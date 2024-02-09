89fae3d8b35ce9f0aeecfc1fe08c3acf65908077
Muhammad Salman <muhdsalm44@gmail.com>
2024-02-09 14:00:30 +0500

    Fix wrong font being passed through

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,11 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("./src/fonts/Montserrat/static/Montserrat-LightItalic.ttf");
-  eleventyConfig.addPassthroughCopy("./src/fonts/Montserrat/static/Montserrat-Regular.ttf");
-  eleventyConfig.addPassthroughCopy("./src/fonts/Montserrat/static/Montserrat-Italic.ttf");
-  eleventyConfig.addPassthroughCopy("./src/fonts/Montserrat/static/Montserrat-Bold.ttf");
+  eleventyConfig.addPassthroughCopy("./src/fonts/Poppins/Poppins-LightItalic.ttf");
+  eleventyConfig.addPassthroughCopy("./src/fonts/Poppins/Poppins-Regular.ttf");
+  eleventyConfig.addPassthroughCopy("./src/fonts/Poppins/Poppins-Italic.ttf");
+  eleventyConfig.addPassthroughCopy("./src/fonts/Poppins/Poppins-Bold.ttf");
+  eleventyConfig.addPassthroughCopy("./src/fonts/Montserrat/static/Montserrat-SemiBold.ttf");
   eleventyConfig.addPassthroughCopy("./src/img");
-  eleventyConfig.addPassthroughCopy("./src/favicon.png");
+  eleventyConfig.addPassthroughCopy("./src/favicon.svg");
   eleventyConfig.addPassthroughCopy("./src/css/output.css")
   eleventyConfig.addPassthroughCopy("./node_modules/bootstrap-icons/font/")
 
