b651a22c1d7188bef44a6344c66b0eb8d2db3fb3
Muhammad Salman <muhdsalm44@gmail.com>
2024-02-09 10:27:21 +0500

    Fix the page system

---
+++ src/pages/404.11ty.js
@@ -0,0 +1,16 @@
+class Four_Zero_Four{
+    data() {
+        return {
+            "permalink": "404.html",
+            "layout": "default.liquid",
+            "title": "Page Not Found",
+            "title_mini": "no kan deteki a note",
+            "description": "",
+            "icon": false,
+            "noScroll": true,
+            "items": []
+        }
+    }
+}
+
+module.exports = Four_Zero_Four
---
+++ src/pages/learn.11ty.js
@@ -0,0 +1,66 @@
+class Learn {
+    data() {
+        return {
+            "permalink": "/learn",
+            "layout": "default.liquid",
+            "title": "Learn Mini",
+            "title_mini": "i etudi a Mini",
+            "description": "Mini is very easy to learn! There are many ways to learn Mini, and you can find all of them linked below!",
+            "icon": false,
+            "noScroll": false,
+            "items": [
+                {
+                    "title": "The Foundational Document",
+                    "title_mini": "base note",
+                    "description": "If you know linguistics, or have dabbled in conlangs and know what is what, then you may want to read the foundational document.",
+                    "button": "Read",
+                    "icon": "bi-journal-richtext",
+                    "url": "https://minilanguage.medium.com/mini-the-minimal-language-3f3710e28166"
+                },
+                {
+                    "title": "Learn Mini in 20 mins",
+                    "title_mini": "i etudi a Mini en 20 minute",
+                    "description": "If you are not the aforementioned linguist, you can read this article, which teaches Mini to normal people.",
+                    "button": "Read",
+                    "icon": "bi-mortarboard",
+                    "url": "https://jprogr.github.io/buku-name/"
+                },
+                {
+                    "title": "Learn Mini through quizzes",
+                    "title_mini": "i etudi a Mini uti esamin",
+                    "description": "If reading documents isn't for you (short as they may be) then you can learn Mini by reading conversations and answering questions based on them.",
+                    "button": "Take the quizzes",
+                    "icon": "bi-question-square",
+                    "url": "https://jprogr.github.io/mini-lessons"
+                },
+                {
+                    "title": "The Mini cheatsheet",
+                    "title_mini": "Mini refere-note",
+                    "description": "If you want a reference guide that fits into a page (yep that's how small Mini is) you can get take a look at the cheatsheet. (Note: Requires joining the Discord server)",
+                    "button": "View",
+                    "icon": "bi-file-earmark",
+                    "url": "https://discord.com/channels/1106636688115105792/1108861705917837493/1161576581844717570"
+                },
+                {
+                    "title": "Mini in my pocket",
+                    "title_mini": "Mini en mi poke",
+                    "description": "You can also use this for reference. It's a small booklet tiny enough to fit into your pocket. (Note: Requires joining the Discord Server)",
+                    "button": "View",
+                    "icon": "bi-calendar2",
+                    "url": "https://discord.com/channels/1106636688115105792/1136734640208621658"
+                },
+                {
+                    "title": "The Mini Dictionary",
+                    "title_mini": "Mini name-buku",
+                    "description": "You can use the Mini dictionary to find out the meanings of all the words in Mini.",
+                    "button": "View",
+                    "icon": "bi-journal-bookmark",
+                    "url": "https://discord.com/channels/1106636688115105792/1136734640208621658"
+                }
+        
+            ]
+        }
+    }
+}
+
+module.exports = Learn
