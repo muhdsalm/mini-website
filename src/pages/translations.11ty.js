b765192b0c9d2b6e08b531d3bdf7045a76fba005
Muhammad Salman <muhdsalm44@gmail.com>
2024-03-27 00:20:40 +0500

    Create the translations page

---
+++ src/pages/translations.11ty.js
@@ -0,0 +1,66 @@
+class Learn {
+    data() {
+        return {
+            "permalink": "/translations/",
+            "layout": "default.liquid",
+            "title": "Translations",
+            "title_mini": "via-linga",
+            "description": "Many texts have been translated into Mini by various people. You can find all these translations below!",
+            "icon": false,
+            "noScroll": false,
+            "items": [
+                {
+                    "title": "The Ants and the Grasshopper",
+                    "title_mini": "Muravi an Kirikiti",
+                    "description": "",
+                    "button": "Read",
+                    "icon": "bi-bug",
+                    "url": "https://minilang.fandom.com/wiki/Litera:Muravi_an_Kirikiti"
+                },
+                {
+                    "title": "Hills like White Elephants",
+                    "title_mini": "Kolina sama Bianka Elefante",
+                    "description": "",
+                    "button": "Read",
+                    "icon": "bi-caret-up",
+                    "url": "https://minilang.fandom.com/wiki/Litera:Kolina_sama_Bianka_Elefante"
+                },
+                {
+                    "title": "If",
+                    "title_mini": "Se",
+                    "description": "",
+                    "button": "Read",
+                    "icon": "bi-question-lg",
+                    "url": "https://jprogr.github.io/mini-lessons"
+                },
+                {
+                    "title": "Sermon on the Mount",
+                    "title_mini": "Edu-Pale on Monte",
+                    "description": "",
+                    "button": "Read",
+                    "icon": "bi-megaphone",
+                    "url": "https://minilang.fandom.com/wiki/Litera:Biblia#5:_Edu-Pale_en_Monte"
+                },
+                {
+                    "title": "The Lake Isle of Innisfree",
+                    "title_mini": "Ila lago de Inisfri",
+                    "description": "",
+                    "button": "Read",
+                    "icon": "bi-water",
+                    "url": "https://minilang.fandom.com/wiki/Litera:Ila_lago_de_Inisfri"
+                },
+                {
+                    "title": "Community",
+                    "title_mini": "komun",
+                    "description": "You can find many more translation in our community",
+                    "button": "Join",
+                    "icon": "bi-discord",
+                    "url": "https://discord.gg/ajA6Z4VrkJ"
+                }
+
+            ]
+        }
+    }
+}
+
+module.exports = Learn
